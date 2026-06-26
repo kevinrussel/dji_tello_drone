67eed3fb0f8eb3c67f8ae989fa91d06ef2301f4b
Kevin Russel <kzone159@gmail.com>
2026-06-25 22:46:05 -0500

    adding code

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from djitellopy import Tello
 import threading
 import queue
 
-
-
-
+def 
 
 
 def main():
     print("hello world")
+
+    tello = Tello()
+    tello.connect()
 
 
 if __name__ == '__main__':
