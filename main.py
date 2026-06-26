8a4e58d832710f9098d536ad023b6a3ae0f5ddae
Kevin Russel <kzone159@gmail.com>
2026-06-25 22:51:54 -0500

    adding code

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ import threading
 import queue
 
 
+class tello_class:
+
+    def __init__(self):
+        self.tello = Tello()
+        self.tello.connect()
 
 
-
-def worker():
-    while True:
-
-
-
-def main():
-    print("hello world")
-
-    tello = Tello()
-    tello.connect()
-
-    udp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-    udp_server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 4 * 1024 * 1024)
-    udp_server_socket.bind(('',8080))
-    udp_server_listen()
+    def worker():
+        while True:
+            pass
 
 
-if __name__ == '__main__':
-    main()
+    def main():
+        print("hello world")
+
+        tello = Tello()
+        tello.connect()
+
+        udp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+        udp_server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 4 * 1024 * 1024)
+        udp_server_socket.bind(('',8080))
+        udp_server_listen()
+
+
+drone = tello_class()
