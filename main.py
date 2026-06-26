528153d14ff0e6d48a0e4be857bf1db422d86d68
Kevin Russel <kzone159@gmail.com>
2026-06-25 22:53:50 -0500

    adding self

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ class tello_class:
     def __init__(self):
         self.tello = Tello()
         self.tello.connect()
-        udp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        udp_server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 4 * 1024 * 1024)
-        udp_server_socket.bind(('',8080))
+        self.udp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.udp_server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 4 * 1024 * 1024)
+        self.udp_server_socket.bind(('',8080))
 
     def worker():
         while True:
