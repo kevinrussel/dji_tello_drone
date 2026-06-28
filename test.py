d2c9fa4c6f3da0d8eac1af1485ac9311fdf0060a
Kevin Russel <kzone159@gmail.com>
2026-06-28 00:04:22 -0500

    adding byte and server address and port

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import time
 import socket
 import struct
 udp_client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
+server_address = 
+port = 
 def takeoff_test():
     header = create_header()
-    
+    command = b"takeoff"
+    message = header + command
+    udp_client_socket.sendto(message(server_address,port))
     
     
     pass
